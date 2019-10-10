e7455e14a9d671b279f4cd59869660676df1a8b9
RayChaung <a0919176442@gmail.com>
2019-10-10 18:02:47 +0800

    first commit

---
+++ NbiotUDP/cmake-build-debug/CMakeFiles/UDP_Server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UDP_Server.dir/sp_funct.cpp.o"
+  "CMakeFiles/UDP_Server.dir/sp_main.cpp.o"
+  "CMakeFiles/UDP_Server.dir/sp_socket.cpp.o"
+  "UDP_Server"
+  "UDP_Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UDP_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NbiotUDP/cmake-build-debug/CMakeFiles/np_single_proc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/np_single_proc.dir/sp_funct.cpp.o"
+  "CMakeFiles/np_single_proc.dir/sp_main.cpp.o"
+  "CMakeFiles/np_single_proc.dir/sp_socket.cpp.o"
+  "np_single_proc"
+  "np_single_proc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/np_single_proc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
